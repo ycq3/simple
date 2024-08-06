c12eec91d996dded7918218f1175282ea6f45f0d
皮皮强 <pipiqiang@pipiqiang.cn>
2024-08-06 16:10:01 +0800

    修改error json输出格式

--- web/errors.go
+++ web/errors.go
@@ -13,9 +13,9 @@ func NewErrorData(code int, text string, data interface{}) *CodeError {
 }
 
 type CodeError struct {
-	Code    int
-	Message string
-	Data    interface{}
+	Code    int 		`json:"code"`
+	Message string		`json:"message"`
+	Data    interface{} 	`json:"data"`
 }
 
 func (e *CodeError) Error() string {
